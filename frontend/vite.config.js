8108be74bec36b1d8d985a620c2cdf61ae44820c
ggO8l8eLPjM6VkZV2nPwfJ18AU7QDOkd <ismaelbenavente95@gmail.com>
2025-12-27 13:45:06 +0000

    Correcciones en vite.config.js para la puesta apunto antes de empezar a desarrollar

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,12 +5,9 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      // Todas las peticiones que empiecen por /api se redirigirán al backend
       '/api': {
         target: 'http://localhost:5000',
-        changeOrigin: true,
-        // Opcional: elimina "/api" de la ruta antes de enviarla al servidor
-        rewrite: (path) => path.replace(/^\/api/, '') 
+        changeOrigin: true
       }
     }
   }
